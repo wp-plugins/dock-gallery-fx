a9c90d1058d428219043f348d51762793419cc15
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-17 12:41:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/dock-gallery-fx/trunk@242295 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dock-gallery-fx.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: Dock Gallery FX
+Plugin URI: http://www.flashxml.net/dock-gallery.html
+Description: An original "Dock Gallery". Completely XML customizable, without using Flash. And it's free!
+Version: 0.1.1
+Author: FlashXML.net
+Author URI: http://www.flashxml.net/
+License: GPL2
+*/
+
+	function dockgalleryfx_get_embed_code($dockgalleryfx_attributes) {
+		$plugin_dir = basename(dirname(__FILE__));
+		$width = (int)$dockgalleryfx_attributes[1];
+		$height = (int)$dockgalleryfx_attributes[2];
+
+		if ($width == 0 || $height == 0) {
+			return '';
+		}
+
+		$swf_embed = array(
+			'width' => $width,
+			'height' => $height,
+			'text' => trim($dockgalleryfx_attributes[3]),
+			'gallery_path' => WP_PLUGIN_URL."/{$plugin_dir}/gallery/",
+			'swf_name' => 'gallery.swf',
+		);
+		$swf_embed['swf_path'] = $swf_embed['gallery_path'].$swf_embed['swf_name'];
+
+		if (!is_feed()) {
+			$embed_code = '<div id="dockgallery-fx">'.$swf_embed['text'].'</div>';
+			$embed_code .= '<script type="text/javascript">';
+			$embed_code .= "swfobject.embedSWF('{$swf_embed['swf_path']}', 'dockgallery-fx', '{$swf_embed['width']}', '{$swf_embed['height']}', '9.0.0.0', '', { folderPath: '{$swf_embed['gallery_path']}' }, { scale: 'noscale', salign: 'tl', wmode: 'transparent', allowScriptAccess: 'sameDomain', allowFullScreen: true }, {});";
+			$embed_code.= '</script>';
+		} else {
+			$embed_code = '<object width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'">';
+			$embed_code .= '<param name="movie" value="'.$swf_embed['swf_path'].'"></param>';
+			$embed_code .= '<param name="scale" value="noscale"></param>';
+			$embed_code .= '<param name="salign" value="tl"></param>';
+			$embed_code .= '<param name="wmode" value="transparent"></param>';
+			$embed_code .= '<param name="allowScriptAccess" value="sameDomain"></param>';
+			$embed_code .= '<param name="allowFullScreen" value="true"></param>';
+			$embed_code .= '<param name="sameDomain" value="true"></param>';
+			$embed_code .= '<param name="flashvars" value="folderPath='.$swf_embed['gallery_path'].'"></param>';
+			$embed_code .= '<embed type="application/x-shockwave-flash" width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'" src="'.$swf_embed['swf_path'].'" scale="noscale" salign="tl" wmode="transparent" allowScriptAccess="sameDomain" allowFullScreen="true" flashvars="folderPath='.$swf_embed['gallery_path'].'"';
+			$embed_code .= '></embed>';
+			$embed_code .= '</object>';
+		}
+
+		return $embed_code;
+	}
+
+	function dockgalleryfx_filter_content($content) {
+		return preg_replace_callback('|\[dock-gallery-fx\s*width\s*=\s*"(\d+)"\s+height\s*=\s*"(\d+)"\s*\](.*)\[/dock-gallery-fx\]|i', 'dockgalleryfx_get_embed_code', $content);
+	}
+
+	function dockgalleryfx_echo_embed_code($width, $height, $div_text = '') {
+		echo dockgalleryfx_get_embed_code(array(1 => $width, 2 => $height, 3 => $div_text));
+	}
+
+	function dockgalleryfx_load_swfobject_lib() {
+		wp_enqueue_script('swfobject');
+	}
+
+	add_filter('the_content', 'dockgalleryfx_filter_content');
+	add_action('init', 'dockgalleryfx_load_swfobject_lib');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Dock Gallery FX ===
+Contributors: flashxml
+Tags: images, photos, widget, post, plugin, posts, sidebar, free, flash, dock, gallery, as3, thumb, effect, thumbs, image, full, screen, text, effects, animation, xml, tooltip, photos, roll, over, out, transition, auto, scroll, css
+Requires at least: 2.8.0
+Tested up to: 2.9.2
+Stable tag: trunk
+
+== Description ==
+
+An original "Dock Gallery". Completely XML customizable, without using Flash. And it's free!
+
+= Main features =
+
+You can integrate it in any website for free without even using Flash. The overall width and height can be customized up to 1680 x 1050 pixels. It's fully customizable and uses the Dock Menu FX component, keeping the same many customizable variables of the menu and adding many gallery properties, text effects and image transitions. You can place the dock menu on any position over the gallery, and you can have it hidden on mouse out. You can configure your XML list of text effects and image transitions. The images in the gallery can be shown according to boxFit, bestFit or forceFit options. The specific properties from the Dock Menu FX like thumbs spacing, scrolling speed, mouse over behaviors and HTML-CSS formatted tooltip are also available.
+
+== Installation ==
+
+Make sure your Wordpress version is equal or greater than 2.8 and your hosting provider is using PHP5.
+
+1. Upload the `dock-gallery-fx` directory along with all its files to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. In the post editor use the following tag to embed the Dock Gallery: `[dock-gallery-fx width="500" height="350"][/dock-gallery-fx]`. Or add `<?php dockgalleryfx_echo_embed_code(500, 350); ?>` in your templates. Don't forget to provide your own width and height values, since 500 and 350 are just examples
+4. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your Dock Gallery](http://www.flashxml.net/dock-gallery.html "Dock Gallery") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `dock-gallery-fx/gallery/settings.xml`
+5. To use your own images, upload them to the `dock-gallery-fx/gallery/images` directory and update the `dock-gallery-fx/gallery/images/thumbs.xml` and `dock-gallery-fx/gallery/images/big.xml` files accordingly
+
+= No Flash support text =
+
+To support visitors without Adobe Flash, you can provide alternative textual content. From the post editor, add the text between `[dock-gallery-fx]` and `[/dock-gallery-fx]`. From the PHP files of your theme, add the text as *the third argument* of the `dockgalleryfx_echo_embed_code()` function call.
+
+== Screenshots ==
+
+1. The Live Demo on [FlashXML.net](http://www.flashxml.net/dock-gallery.html "Dock Gallery") is the utility that helps easily customize your Dock Gallery to fit all of your needs.
